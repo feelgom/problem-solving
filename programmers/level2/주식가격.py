8070da02cc2da98979d252cb5bee2fc517ba3802
Feelgom <littlestork4@gmail.com>
2022-07-24 23:56:23 +0900

    22.07.24 cleaning programmers problem codes

--- programmers/level2/주식가격.py
+++ programmers/level2/주식가격.py
@@ -1,13 +1,23 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42584
 
 def solution(prices):
-    answer = []
+    answer = [-999] * len(prices)
     queue = [[0,-1]]
-    for ind, p in enumerate(prices):
-        if p >= queue[-1][0]:
-            queue.append([p, ind])
+    for ind, price in enumerate(prices):
+        if price >= queue[-1][0]:
+            queue.append([price, ind])
         else:
-            while p < queue[-1][0]:
-                answer.append(p)
+            while price < queue[-1][0]:
+                _, index = queue.pop()
+                answer[index]= ind - index
+            queue.append([price,ind])
+
+    for _, index in queue:
+        answer[index] = ind- index
 
     return answer
+
+if __name__=="__main__":
+    prices = [1, 2, 3, 2, 3]
+    answer = [4, 3, 1, 1, 0]
+    print(solution(prices)==answer)
--- programmers/level2/큰수만들기.py
+++ programmers/level2/큰수만들기.py
@@ -1,39 +1,25 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42883
-
+    
 def solution(number, k):
-    ans_list = []
     num_list = list(number)
     num_list = [int(elem) for elem in num_list]
+    ans_list = []
+    while len(num_list) > k and k > 0:
+        if 9 in num_list[:k+1]:
+            num_max = 9
+        else:
+            num_max = max(num_list[:k+1])
+
+        ans_list.append(str(num_max))
+        idx = num_list.index(num_max)
+        num_list = num_list[idx+1:]
+        k -= idx
     
-    while 1:
-        for i in range(len(num_list)-1):
-            if num_list[i] >= num_list[i+1]:
-                ans_list.append(num_list[i])
-                k-=1
-            if k == 0:
-                break
-        num_list = ans_list
+    if k == 0:
+        for elem in num_list:
+            ans_list.append(str(elem))
     
-    ans_list = [str(elem) for elem in ans_list]
     return "".join(ans_list)
-
-    
-# def solution(number, k):
-#     num_list = list(number)
-#     num_list = [int(elem) for elem in num_list]
-#     ans_list = []
-#     while len(num_list) > k:
-#         if 9 in num_list[:k+1]:
-#             num_max = 9
-#         else:
-#             num_max = max(num_list[:k+1])
-
-#         ans_list.append(str(num_max))
-#         idx = num_list.index(num_max)
-#         num_list = num_list[idx+1:]
-#         k -= idx
-    
-#     return "".join(ans_list)
 
 
 if __name__=="__main__":
