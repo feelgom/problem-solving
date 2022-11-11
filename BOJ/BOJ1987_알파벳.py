5528ed9830b2ff66f891bb344bf2bb4acebdbc73
Feelgom <littlestork4@gmail.com>
2022-11-11 23:55:23 +0900

    22.11.11 BOJ 1 problem solving

--- BOJ/BOJ1987_알파벳.py
+++ BOJ/BOJ1987_알파벳.py
@@ -1,10 +1,33 @@
 #%%
-
-R, C = map(int,input().split())
+# R, C = map(int,input().split())
 
 world = []
 visit = []
-for _ in range(R):
-    world.append(input())
+# for _ in range(R):
+#     world.append(input())
     
-visit.append(world[0][0])
+# score = [[0]*C]*R
+world =[
+'HFDFFB',
+'AJHGDH',
+'DGAGEH']
+
+y, x= 0, 0
+move = [[1,0], [-1,0], [0,1], [0,-1]]    
+
+def DFS(x, y, visit):
+    visit.append(world[y][x])
+    max_score = len(visit)
+    for dx,dy in move:
+        nx = x+dx
+        ny = y+dy
+        if nx >=0 and nx < C and ny >= 0 and ny < R and world[ny][nx] not in visit:
+            score_ = DFS(nx, ny, visit)
+            print(score_, max_score)
+            if score_ > max_score:
+                max_score = score_
+            
+    return max_score    
+
+
+
