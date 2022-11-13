a24c40b2c2908aa2e24cff4435f361fdcc6d7240
Feelgom <littlestork4@gmail.com>
2022-11-14 00:12:21 +0900

    22.11.14 BOJ 1 problem fix

--- BOJ/BOJ2357_최솟값과최댓값.py
+++ BOJ/BOJ2357_최솟값과최댓값.py
@@ -1,42 +1,46 @@
 #%%
-# 단순 리스트로 만들시 시간초과가 발생한다.
-# N, M = map(int,input().split())
-N, M = 10, 4
-numbers = [75,30, 100,38,50,51,52,20,81,5]
-# for _ in range(N):
-#     numbers.append(int(input()))
-
-
-# for _ in range(M):
-    # min_, max_ = map(int, input().split())
-    # lis2 = numbers[min_-1:max_]
-    # print(min(lis2), max(lis2))
-    
-# segment tree (구간합트리)를 만들어서 구하자
-length = len(numbers)
-i = 0
-while 2**i <= length:
-    i+=1
-tree_length = 2**(i+1)
-tree = [0]*tree_length
+from math import inf
+import sys
 
 def make_tree(start,end,node):
     if start == end:
-        tree[node] = numbers[start]
+        tree[node] = [numbers[start],numbers[start]]
         return tree[node]
     mid = (start+end)//2
-    
-    tree[node] = make_tree(start,mid,node*2)+ make_tree(mid+1,end,node*2+1)
+    a = make_tree(start,mid,node*2)
+    b = make_tree(mid+1,end,node*2+1)
+    tree[node] = [min(a[0],b[0]), max(a[1],b[1])]
     return tree[node]
 
-def tree_sum(start,end, node, left,right):
+def get_minmax(start, end, node, left,right):
     if start>right or end<left:
-        return 0
+        return [inf, -inf]
     if start>=left and end<=right:
         return tree[node]
     
     mid = (start+end)//2
-    return tree_sum(start,mid,node*2,left,right) + tree_sum(mid+1,end,node*2+1,left,right)
+    a = get_minmax(start,mid,node*2,left,right)
+    b = get_minmax(mid+1,end,node*2+1,left,right)
+    
+    return [min(a[0],b[0]), max(a[1],b[1])]
 
-make_tree(0,M-1,1)
+# 단순 리스트로 만들시 시간초과가 발생한다.
+# segment tree (구간합트리)를 만들어서 구하자
 
+N, M = map(int,input().split())
+numbers = []
+for _ in range(N):
+    num = int(sys.stdin.readline().strip())
+    numbers.append(num)
+length = len(numbers)
+tree_length = 1
+while tree_length <= length:
+    tree_length *= 2
+tree_length *= 2
+tree = [[inf,-inf]]*tree_length
+
+make_tree(0,N-1,1)
+for _ in range(M):
+    mi, ma = map(int, sys.stdin.readline().strip().split())
+    ans = get_minmax(0,N-1,1, mi-1, ma-1)
+    print(*ans)
