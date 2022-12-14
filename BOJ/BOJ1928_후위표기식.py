91dbd028c49422de263c3340e303ecd0107f9796
Feelgom <littlestork4@gmail.com>
2022-12-14 22:19:27 +0900

    22.12.14 BOJ 1 problem solving

---
+++ BOJ/BOJ1928_후위표기식.py
@@ -0,0 +1,14 @@
+import queue
+
+def func(q):
+    
+
+    return 0     
+    
+    
+if __name__=="__main__":
+    string = input()
+    q = queue.deque(string)
+    answer = func(q)
+    
+    print(answer)
