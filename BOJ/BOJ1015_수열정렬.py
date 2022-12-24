9e1dfbae7d3fc6e6050646fd978aea9b19f6358c
Feelgom <littlestork4@gmail.com>
2022-12-24 23:44:02 +0900

    22.12.24 BOJ 1 problem solving

--- BOJ/BOJ1015_수열정렬.py
+++ BOJ/BOJ1015_수열정렬.py
@@ -6,9 +6,9 @@ lis2 = []
 for i, a in enumerate(lis):
     lis2.append([a, i])
 
-lis2.sort(reverse=True)
-lis3 = []
-for _, i in lis2:
-    lis3.append(i)
+lis2.sort()
+lis3 = [0]*N
+for i, item in enumerate(lis2):
+    lis3[item[1]]= i
 
 print(*lis3)
