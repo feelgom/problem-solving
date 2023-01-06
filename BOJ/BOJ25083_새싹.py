8e27c2783b2fd778e8cc7e9c350dbf181eba699b
Feelgom <littlestork4@gmail.com>
2023-01-07 00:52:48 +0900

    23.01.07 BOJ 1 problem solving

---
+++ BOJ/BOJ25083_새싹.py
@@ -0,0 +1,12 @@
+print("         ,r'", end="")
+print('"', end="")
+print("7")
+
+print("r`-_   ,'  ,/")
+
+print(' \\. ". L_r', end="")
+print("'")
+
+print('   `~\\/')
+print('      |')
+print('      |')
