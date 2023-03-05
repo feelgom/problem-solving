892413829f94f1f5abd5189210b4e348f8d56afa
Feelgom <littlestork4@gmail.com>
2023-03-06 01:01:58 +0900

    23.03.06 BOJ 3 problems solving

---
+++ BOJ/BOJ10814_나이순정렬.py
@@ -0,0 +1,9 @@
+N = int(input())
+
+members = []
+for _ in range(N):
+    members.append(list(input().split()))
+    
+members.sort(key=lambda x: (int(x[0])))
+for member in members:
+    print(*member)
--- BOJ/BOJ2108_통계학.py
+++ BOJ/BOJ2108_통계학.py
@@ -3,27 +3,26 @@ input = sys.stdin.readline
 
 N = int(input())
 lis = []
-counts = [0] * 8001
+counts = [0] * 100000
 for _ in range(N):
     num = int(input())
     lis.append(num)
-    counts[num] += 1
+    counts[num+4000] += 1
         
 lis.sort()
 mean = round(sum(lis)/len(lis))
 median = lis[N//2]
 
 range_ = lis[-1]-lis[0]
+
 max_count = max(counts)
 mode = counts.index(max_count)
-if mode > 4000:
-    mode -= 8001
-    
+
 counts[mode] = 0
 if max_count in counts:
     mode = counts.index(max_count)
-if mode > 4000:
-    mode -= 8001
+
+mode -= 4000
 
 print(mean)
 print(median)
