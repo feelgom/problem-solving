cc7481c46acd99307dab6c5471a1ca28922f320f
Feelgom <littlestork4@gmail.com>
2023-03-22 23:58:35 +0900

    23.03.22 BOJ 1 problem solving

---
+++ BOJ/BOJ13241_최소공배수.py
@@ -0,0 +1,13 @@
+a, b = map(int,input().split())
+big = max(a,b)
+small = min(a,b)
+
+while True:
+    remainder = big % small
+    if remainder == 0:
+        GCF = small
+        break
+    big = small
+    small = remainder
+    
+print(int(a*b/GCF))
